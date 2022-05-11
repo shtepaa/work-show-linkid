ee45c69124c18f3f7a55e556483ee74c71ce9049
Anastasiia Shtepa <anastasiia.shtepa1990@gmail.com>
2022-05-11 09:26:17 +0300

    App add

--- les01/test.js
+++ les01/test.js
@@ -1,25 +1,52 @@
-const products = [
-  { id: 1, title: "Shirt", price: 150, image: "../solut/images/featured/1.jpg"},
-  { id: 2, title: "Socks", price: 50, image: "../solut/images/featured/2.jpg"},
-  { id: 3, title: "Jacket", price: 350, image: "../solut/images/featured/3.jpg"},
-  { id: 4, title: "Shoes", price: 250, image: "../solut/images/featured/4.jpg"},
-];
+// const products = [
+//   { id: 1, title: "Shirt", price: 150, image: "../solut/images/featured/1.jpg"},
+//   { id: 2, title: "Socks", price: 50, image: "../solut/images/featured/2.jpg"},
+//   { id: 3, title: "Jacket", price: 350, image: "../solut/images/featured/3.jpg"},
+//   { id: 4, title: "Shoes", price: 250, image: "../solut/images/featured/4.jpg"},
+// ];
 
-const getProductHTMLString = (title, price, image) => {
-  return ` <div class="product-item">
-           <h3>${title}</h3>
-           <p>${price}</p>
-           <img src="${image}" alt="">
-          <button class="by-btn">Add to cart</button>
-          </div>`;
-};
+// const getProductHTMLString = (title, price, image) => {
+//   return ` <div class="product-item">
+//            <h3>${title}</h3>
+//            <p>${price}</p>
+//            <img src="${image}" alt="">
+//           <button class="by-btn">Add to cart</button>
+//           </div>`;
+// };
 
-const renderProducts = (productList) => {
-  const list = productList.map((good) =>
-    getProductHTMLString(good.title, good.price, good.image)
-  );
+// const renderProducts = (productList) => {
+//   const list = productList.map((good) =>
+//     getProductHTMLString(good.title, good.price, good.image)
+//   );
 
-  document.querySelector(".product-item").innerHTML = list.join('');
-};
+//   document.querySelector(".product-item").innerHTML = list.join('');
+// };
 
-renderProducts(products);
+// renderProducts(products);
+
+class GoodsList {
+  constructor() {
+    this.goods = [];
+  }
+  fetchGoods () {
+    this.goods = [
+      { title: 'Shirt', price: 150 },
+      { title: 'Socks', price: 50 },
+      { title: 'Jacket', price: 350 },
+      { title: 'Shoes', price: 250 },
+    ];
+  }
+  render() {
+    let listHtml = '';
+    this.goods.forEach(good => {
+      const goodItem = new GoodsItem(good.title, good.price);
+      listHtml += goodItem.render();
+    });
+    document.querySelector('.goods-list').innerHTML = listHtml;
+  }
+  
+
+}
+const list = new GoodsList();
+  list.fetchGoods();
+  list.render();
