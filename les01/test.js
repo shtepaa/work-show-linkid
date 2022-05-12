918f254343f531c5d9c82202e2d4cf5ad9941eb7
Anastasiia Shtepa <anastasiia.shtepa1990@gmail.com>
2022-05-12 14:31:46 +0300

    change app code

--- les01/test.js
+++ les01/test.js
@@ -24,29 +24,79 @@
 
 // renderProducts(products);
 
+// class GoodsList {
+//   constructor() {
+//     this.goods = [];
+//   }
+//   fetchGoods () {
+//     this.goods = [
+//       { title: 'Shirt', price: 150 },
+//       { title: 'Socks', price: 50 },
+//       { title: 'Jacket', price: 350 },
+//       { title: 'Shoes', price: 250 },
+//     ];
+//   }
+//   render() {
+//     let listHtml = '';
+//     this.goods.forEach(good => {
+//       const goodItem = new GoodsItem(good.title, good.price);
+//       listHtml += goodItem.render();
+//     });
+//     document.querySelector('.goods-list').innerHTML = listHtml;
+//   }
+
+// }
+// const list = new GoodsList();
+//   list.fetchGoods();
+//   list.render();
+function makeGETRequest(url, callback) {
+  var xhr;
+  if (window.XMLHttpRequest) {
+    xhr = new XMLHttpRequest();
+  } else if (window.ActiveXObject) {
+    xhr = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      callback(xhr.responseText);
+    }
+  };
+  xhr.open("GET", url, true);
+  xhr.send();
+}
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 class GoodsList {
-  constructor() {
-    this.goods = [];
+  // ...
+  fetchGoods() {
+  makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
+  this.goods = JSON.parse(goods);
+  })
   }
-  fetchGoods () {
-    this.goods = [
-      { title: 'Shirt', price: 150 },
-      { title: 'Socks', price: 50 },
-      { title: 'Jacket', price: 350 },
-      { title: 'Shoes', price: 250 },
-    ];
+  // ...
   }
-  render() {
+  class GoodsItem {
+    // ...
+    render() {
+    return `<div
+    class="goods-item"><h3>${this.product_name}</h3><p>${this.price}</p></div>`;
+    }
+    }
+    class GoodsList {
+    // ...
+    fetchGoods() {
+    makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
+    this.goods = JSON.parse(goods);
+    })
+    }
+    render() {
     let listHtml = '';
     this.goods.forEach(good => {
-      const goodItem = new GoodsItem(good.title, good.price);
-      listHtml += goodItem.render();
-    });
-    document.querySelector('.goods-list').innerHTML = listHtml;
-  }
-  
-
+    const goodItem = new GoodsItem(good.product_name, good.price);
+    listHtml += goodItem.render();
+});
+document.querySelector('.goods-list').innerHTML = listHtml;
+}
 }
 const list = new GoodsList();
-  list.fetchGoods();
-  list.render();
+list.fetchGoods();
+list.render();
