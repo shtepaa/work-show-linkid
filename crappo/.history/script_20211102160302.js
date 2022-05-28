c3aef54899dd02cc237e0df6cd3fc55ad94809fb
Anastasiia Shtepa <anastasiia.shtepa1990@gmail.com>
2022-05-28 17:48:18 +0300

    new voice app

--- crappo/.history/script_20211102160302.js
+++
@@ -1,9 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const nav = document.querySelector(".nav");
-
-hamburger.addEventListener("click", mobileMenu);
-
-function mobileMenu() {
-    hamburger.classList.toggle("active");
-    nav.classList.toggle("active");
-}
